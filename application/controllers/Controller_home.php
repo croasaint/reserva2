85ad1e0758a6cb355ac2149caf02606b9843299d
Abraham Navarro <anavarro@teravisiontech.com>
2020-06-05 19:52:50 -0400

    test

--- application/controllers/Controller_home.php
+++ application/controllers/Controller_home.php
@@ -8,6 +8,7 @@ class Controller_home extends CI_Controller {
 
 }
 
+
 public function index(){
   $data['services'] = $this->Service_model->showService();
   $this->load->view('layout/layout_home', $data);
